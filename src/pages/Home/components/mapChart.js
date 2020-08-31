59aed9bcf5b60e8a135002a6fa6286a9b10f538e
Lebedev Alexandr <s.lebedev.2011@bk.ru>
2020-08-31 21:29:05 +0300

    update map-chard; add apiService

--- src/pages/Home/components/mapChart.js
+++ src/pages/Home/components/mapChart.js
@@ -1,4 +1,5 @@
-import React, { memo } from "react";
+import React from "react";
+import {scaleLinear} from "d3-scale";
 import {
     ComposableMap,
     Sphere,
@@ -7,64 +8,111 @@ import {
     Geographies,
     Geography
 } from "react-simple-maps";
+import PropTypes from "prop-types";
+
 
 const geoUrl =
     "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
+
 
 const rounded = num => {
     if (num > 1000000000) {
         return Math.round(num / 100000000) / 10 + "Bn";
     } else if (num > 1000000) {
         return Math.round(num / 100000) / 10 + "M";
-    } else {
+    } else if (num > 100) {
         return Math.round(num / 100) / 10 + "K";
+    } else {
+        return num;
     }
 };
 
-const MapChart = ({ setTooltipContent }) => {
-    const height = 600;
-    const width = 1200
-    const scale = 200;
+export default class MapChart extends React.Component {
+    static propTypes = {
+        setTooltipContent: PropTypes.func.isRequired,
+        countriesData: PropTypes.arrayOf(PropTypes.shape({
+            countryCode: PropTypes.string.isRequired,
+            value: PropTypes.number.isRequired,
+        })),
+    }
 
-    return (
-        <ComposableMap data-tip="" width={width} height={height} projectionConfig={{ scale: scale }}>
-            <ZoomableGroup translateExtent={[[0, 0], [width, height]]}>
-                <Sphere stroke="#DDD" />
-                <Graticule stroke="#DDD" />
-                <Geographies geography={geoUrl}>
-                    {({ geographies }) =>
-                        geographies.map(geo => (
-                            <Geography
-                                key={geo.rsmKey}
-                                geography={geo}
-                                onMouseEnter={() => {
-                                    const { NAME, POP_EST } = geo.properties;
-                                    setTooltipContent(`${NAME} — ${rounded(POP_EST)}`);
-                                }}
-                                onMouseLeave={() => {
-                                    setTooltipContent("");
-                                }}
-                                style={{
-                                    default: {
-                                        fill: "#D6D6DA",
-                                        outline: "none"
-                                    },
-                                    hover: {
-                                        fill: "#3082d2",
-                                        outline: "none"
-                                    },
-                                    pressed: {
-                                        fill: "#3082d2",
-                                        outline: "none"
-                                    }
-                                }}
-                            />
-                        ))
-                    }
-                </Geographies>
-            </ZoomableGroup>
-        </ComposableMap>
-    );
+    static defaultProps = {
+        countriesData: [],
+    }
+    componentWillReceiveProps(nextProps) {
+        console.log(nextProps);
+    }
+
+    render() {
+        const countriesData = this.props.countriesData;
+
+        const allCountriesValues = countriesData.map((countryData => countryData.value))
+        const maxValue = Math.max(...allCountriesValues);
+        const minValue = Math.min(...allCountriesValues);
+
+        const colorScale = scaleLinear()
+            .domain([minValue, maxValue])
+            .range(["#ffedea", "#ff5233"]);
+
+
+        const height = 1200;
+        const width = 2400
+        const scale = 400;
+
+        return (
+            <ComposableMap data-tip="" width={width} height={height} projectionConfig={{scale: scale}}>
+                <ZoomableGroup translateExtent={[[0, 0], [width, height]]}>
+                    <Sphere stroke="#DDD"/>
+                    <Graticule stroke="#DDD"/>
+                    <Geographies geography={geoUrl}>
+                        {({geographies}) =>
+                            geographies.map(geo => {
+                                const countryData = countriesData.find((country) => country.countryCode === geo.properties.ISO_A2)
+
+                                let color;
+                                if (countryData === undefined) {
+                                    color = "#D6D6DA";
+                                } else {
+                                    color = colorScale(countryData.value);
+                                }
+
+
+                                return <Geography
+                                    key={geo.rsmKey}
+                                    geography={geo}
+                                    onMouseEnter={() => {
+                                        const {NAME} = geo.properties;
+                                        this.props.setTooltipContent(`${NAME} — ${countryData !== undefined ? rounded(countryData.value) : 'N/A'}`);
+                                    }}
+                                    onMouseLeave={() => {
+                                        this.props.setTooltipContent("");
+                                    }}
+                                    style={{
+                                        default: {
+                                            fill: color,
+                                            outline: "none",
+                                            stroke: '#b1b1b1',
+                                            strokeWidth: 0.5,
+                                            transition: "500ms",
+                                        },
+                                        hover: {
+                                            fill: "#e4e4e4",
+                                            outline: "none",
+                                            stroke: '#b1b1b1',
+                                            strokeWidth: 0.5,
+                                            transition: "500ms",
+                                        },
+                                        pressed: {
+                                            fill: "#e4e4e4",
+                                            outline: "none"
+                                        }
+                                    }}
+                                />
+                            })
+                        }
+                    </Geographies>
+                </ZoomableGroup>
+            </ComposableMap>
+        );
+    }
 };
-
-export default memo(MapChart);
