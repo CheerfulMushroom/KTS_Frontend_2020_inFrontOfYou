93bb17ad1abd5c31a3b196962243422eff51286c
Lebedev Alexandr <s.lebedev.2011@bk.ru>
2020-09-12 12:59:10 +0300

    move example chart data to exampleData.js

--- src/pages/Home/components/chart.js
+++ src/pages/Home/components/chart.js
@@ -1,33 +1,8 @@
 import React from "react";
 import {Chart} from "react-charts";
+import {chartData} from "./exampleData";
 
 export default function MyChart() {
-    const data = React.useMemo(
-        () => [
-            {
-                label: "Series 1",
-                data: [
-                    [0, 1],
-                    [1, 2],
-                    [2, 4],
-                    [3, 2],
-                    [4, 7],
-                ],
-            },
-            {
-                label: "Series 2",
-                data: [
-                    [0, 3],
-                    [1, 1],
-                    [2, 5],
-                    [3, 6],
-                    [4, 4],
-                ],
-            },
-        ],
-        [],
-    );
-
     const axes = React.useMemo(
         () => [
             {primary: true, type: "linear", position: "bottom"},
@@ -42,7 +17,7 @@ export default function MyChart() {
                 height: "300px",
             }}
         >
-            <Chart data={data} axes={axes} />
+            <Chart data={chartData} axes={axes} />
         </div>
     );
 }
